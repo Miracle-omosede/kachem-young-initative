b95a44e551aac1d413fb7fa8a6d5f1daecee355b
Miracle-omosede <miracleigbinoghene@gmail.com>
2024-04-17 23:01:05 +0100

    First Push

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import Container from '@/app/Container'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <Container>
+        
+    </Container>
+  )
+}
+
+export default Navbar
