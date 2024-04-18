4c6a926f4af80b5bcb0861621c84822a70de60ae
Miracle-omosede <miracleigbinoghene@gmail.com>
2024-04-18 23:08:44 +0100

    fixed hero mobile

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <div className='hero-bg h-screen flex items-center justify-center'>
        <div className='w-auto md:max-w-[1180px] flex flex-col gap-[30px]'>
-        <h1 className='font-poppins text-[80px] font-bold text-white text-center leading-[6rem]'>
+        <h1 className='font-poppins text-[80px] font-bold text-white text-center leading-[5.4rem]'>
         Believing in a sustainable future through faith, hardwork, and resilience. 
         </h1>
         <div className='flex justify-center items-center'>
